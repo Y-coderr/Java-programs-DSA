25d68b44c8487d615b4523327d6560cf2cc24a4c
Yash Kadam <kadamyash6112@gmail.com>
2025-05-19 19:26:52 +0530

    copy

--- BinaryTreeTraversal.java
+++ BinaryTreeTraversal.java
@@ -1,30 +1,54 @@
 public class BinaryTreeTraversal{
-   class Node {
-    int data;
-    Node left, right;
+    static class Node{
+        int data;
+        Node left;
+        Node right;
 
-    public Node(int item) {
-        data = item;
-        left = right = null;
+        Node (int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+         
     }
+    static class BinaryTree{
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            }
+             Node newNode = new Node(nodes[idx]);
+             newNode.left=buildTree(nodes);
+             newNode.right=buildTree(nodes);
+
+             return newNode;
+        }
+
+        static void preOrder(Node root){
+            System.out.println(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        static void inOrder(Node root){
+            preOrder(root.left);
+            System.out.println(root.data+" "); 
+            preOrder(root.right);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+
+        tree.preOrder(root);
+    }
+    
 }
 
-    public static Node preOrdered(Node root){
-        if(root == null){
-            return null;
-        }
-        System.out.print(root.data + " ");
-        preOrdered(root.left);
-        preOrdered(root.right);
-    }
-
-    public static Node levelOrderTraversal(Node root){
-        if(root == null){
-            return null;
-        }
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            
-    }
-}
+   
+    
