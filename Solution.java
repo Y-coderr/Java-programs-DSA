96655fcefc60be88c5643e691e136ab1bbe89b89
Yash Kadam <kadamyash6112@gmail.com>
2025-07-13 16:33:16 +0530

    connecting cities Graph

--- Solution.java
+++ Solution.java
@@ -1,15 +1,40 @@
 class Solution {
-    public String convertToTitle(int columnNumber) {
+    public boolean backspaceCompare(String s, String t) {
         StringBuilder sb = new StringBuilder();
-        while(columnNumber > 0){
-            columnNumber--;
-            int rem = columnNumber % 26;
-            char ch = (char) ('A' + rem);
-            sb.append(ch);
-            columnNumber /= 26;
+        StringBuilder tb = new StringBuilder();
+
+        Stack<Character> sc = new Stack<>();
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='#'){
+                sc.push(s.charAt(i));
+            }else{
+                sc.pop();
+            }
         }
 
-        return sb.reverse().toString();
-    }
+        for(int i=0;i<t.length();i++){
+            if(t.charAt(i)!='#'){
+                st.push(t.charAt(i));
+            }else{
+                st.pop();
+            }
+        }
 
+        while(!sc.isEmpty()){
+            sb.append(sc.pop());
+        }
+        while(!st.isEmpty()){
+            tb.append(st.pop());
+        }
+
+        String ans1 = sb.toString();
+        String ans2= tb.toString();
+
+        if(ans1 == ans2){
+            return true;
+        }
+        return false;
+    }
 }
